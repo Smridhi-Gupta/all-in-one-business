b5650d70b876c70d16eeea175a6b9d54e6106dfc
smridhi <smridhi106@gmail.com>
2025-09-23 22:29:39 +0530

    init

---
+++ src/app/db/navigationData.js
@@ -0,0 +1,16 @@
+export const navigationData = [
+  {
+    label: "Home",
+    href: "/",
+    isActive: true,
+  },
+  {
+    label: "Services",
+    href: "/services",
+  },
+
+  {
+    label: "Contact",
+    href: "/contact",
+  },
+];
