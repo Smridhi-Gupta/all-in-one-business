88b3604f127ba60c781e27abb99823ee9a53754f
smridhi <smridhi106@gmail.com>
2026-01-15 04:21:31 +0530

    init

--- src/app/change-of-status/page.jsx
+++ src/app/change-of-status/page.jsx
@@ -1,11 +1,11 @@
 import FAQSection from '@/components/home/sections/FAQSection'
-import Disclaimer from '@/components/services/sections/Disclaimer'
-import Hero from '@/components/services/sections/Hero'
-import TakeControl from '@/components/services/sections/TakeControl'
-import WhoWeHelp from '@/components/services/sections/WhoWeHelp'
-import WhyChooseEmbtel from '@/components/services/sections/WhyChooseEmbtel'
-import WhyChooseEmbtel2 from '@/components/services/sections/WhyChooseEmbtel2'
-import WhyThisMatters from '@/components/services/sections/WhyThisMatters'
+import Disclaimer from '@/components/change-of-status/sections/Disclaimer'
+import Hero from '@/components/change-of-status/sections/Hero'
+import TakeControl from '@/components/change-of-status/sections/TakeControl'
+import WhoWeHelp from '@/components/change-of-status/sections/WhoWeHelp'
+import WhyChooseEmbtel from '@/components/change-of-status/sections/WhyChooseEmbtel'
+import WhyChooseEmbtel2 from '@/components/change-of-status/sections/WhyChooseEmbtel2'
+import WhyThisMatters from '@/components/change-of-status/sections/WhyThisMatters'
 import React from 'react'
 
 const page = () => {
